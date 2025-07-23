7f02b0e227da10a1185c359a581fd4b007d85efb
nczajak <nczajak@student.agh.edu.pl>
2025-07-23 16:47:00 +0200

    setting lights and validating moves

--- src/test/java/test/RoadQueueTest.java
+++ src/test/java/test/RoadQueueTest.java
@@ -13,7 +13,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class RoadQueueTest {
 
     @Test
-    public void validRoadWithLongestQueue(){
+    public void getSortedRoads(){
         Road road1 = new Road(RoadDirection.NORTH);
         Road road2 = new Road(RoadDirection.EAST);
         Road road3 = new Road(RoadDirection.SOUTH);
@@ -27,26 +27,28 @@ class RoadQueueTest {
 
         RoadQueue roadQueue = new RoadQueue(roads);
 
-        assertEquals(road1,roadQueue.getRoadWithLongestQueue());
+        assertEquals(roads,roadQueue.getSortedRoads());
+        assertEquals(road1,roadQueue.getSortedRoads().getFirst());
 
         Vehicle vehicle1 = new Vehicle("vehicle1",RoadDirection.EAST,RoadDirection.WEST);
         road2.addVehicle(vehicle1);
 
-        assertEquals(road2, roadQueue.getRoadWithLongestQueue());
+        assertEquals(road2, roadQueue.getSortedRoads().getFirst());
 
         Vehicle vehicle2 = new Vehicle("vehicle2",RoadDirection.SOUTH,RoadDirection.WEST);
         Vehicle vehicle3 = new Vehicle("vehicle3",RoadDirection.SOUTH,RoadDirection.WEST);
 
         road3.addVehicle(vehicle2);
+        assertEquals(road2,roadQueue.getSortedRoads().getFirst());
         road3.addVehicle(vehicle3);
 
-        assertEquals(road3,roadQueue.getRoadWithLongestQueue());
+        assertEquals(road3,roadQueue.getSortedRoads().getFirst());
 
         Vehicle vehicle4 = new Vehicle("vehicle4",RoadDirection.EAST,RoadDirection.SOUTH);
 
         road2.addVehicle(vehicle4);
 
-        assertEquals(road3,roadQueue.getRoadWithLongestQueue());
+        assertEquals(road3,roadQueue.getSortedRoads().getFirst());
 
     }
 
--- src/test/java/test/RoadTest.java
+++ src/test/java/test/RoadTest.java
@@ -33,9 +33,49 @@ class RoadTest {
     @Test
     public void invalidVehiclesAmount(){
         Road road = new Road(RoadDirection.NORTH);
-        assertThrows(EmptyRoadException.class,()->{
-            road.moveVehicle();
-        });
+        assertThrows(EmptyRoadException.class, road::moveVehicle);
+    }
+
+
+    @Test
+    public void validEmptyRoadPriority(){
+        Road road = new Road(RoadDirection.NORTH);
+
+        assertEquals(0,road.getPriority());
+    }
+
+    @Test
+    public void validRoadPriorityWithSomeVehicles(){
+        Road road = new Road(RoadDirection.NORTH);
+        Vehicle vehicle1 = new Vehicle("vehicle1",RoadDirection.NORTH,RoadDirection.WEST);
+
+        road.addVehicle(vehicle1);
+        road.addVehicle(vehicle1);
+
+        assertEquals(2,road.getPriority());
+    }
+
+    @Test
+    public void validRoadPriorityWithSomeVehiclesAndWaitingTime(){
+        Road road = new Road(RoadDirection.NORTH);
+        Vehicle vehicle1 = new Vehicle("vehicle1",RoadDirection.NORTH,RoadDirection.WEST);
+        Vehicle vehicle2 = new Vehicle("vehicle2",RoadDirection.NORTH,RoadDirection.WEST);
+
+        road.addVehicle(vehicle1);
+        road.addVehicle(vehicle2);
+
+        vehicle1.increaseWaitingTime();
+
+        assertEquals(3,road.getPriority());
+
+        vehicle1.increaseWaitingTime();
+        vehicle2.increaseWaitingTime();
+
+        assertEquals(4, road.getPriority());
+
+        road.moveVehicle();
+
+        assertEquals(2,road.getPriority());
     }
 
 
