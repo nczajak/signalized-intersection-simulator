fb6281001227522c7ee1eaf69c9a8a297e5451e2
nczajak <nczajak@student.agh.edu.pl>
2025-07-24 19:38:41 +0200

    crossroad test

---
+++ src/test/java/test/CrossRoadTest.java
@@ -0,0 +1,36 @@
+package test;
+
+import Model.CrossRoad;
+import Model.Vehicle;
+import org.junit.jupiter.api.Test;
+import Enum.RoadDirection;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CrossRoadTest {
+
+    @Test
+    public void getRoadsSortedByPriority(){
+        CrossRoad crossRoad = new CrossRoad();
+        Vehicle vehicle1 = new Vehicle("vehicle1", RoadDirection.NORTH,RoadDirection.SOUTH);
+        crossRoad.putVehicleOnValidRoad(vehicle1);
+
+        assertEquals(RoadDirection.NORTH,crossRoad.getRoadsSortedByPriority().getFirst().getDirection());
+
+        Vehicle vehicle2 = new Vehicle("vehicle2", RoadDirection.WEST,RoadDirection.SOUTH);
+        crossRoad.putVehicleOnValidRoad(vehicle2);
+
+        assertEquals(RoadDirection.NORTH,crossRoad.getRoadsSortedByPriority().getFirst().getDirection());
+
+        Vehicle vehicle3 = new Vehicle("vehicle3", RoadDirection.WEST,RoadDirection.SOUTH);
+        crossRoad.putVehicleOnValidRoad(vehicle3);
+
+        assertEquals(RoadDirection.WEST,crossRoad.getRoadsSortedByPriority().getFirst().getDirection());
+
+        crossRoad.moveVehicles();
+
+        assertEquals(RoadDirection.WEST,crossRoad.getRoadsSortedByPriority().getFirst().getDirection());
+
+    }
+
+}
