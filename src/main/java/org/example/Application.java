6c87d55fb8f49ab2454e52f900af76a902538e5f
nczajak <nczajak@student.agh.edu.pl>
2025-07-27 21:22:28 +0200

    starting app by Application class

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -1,11 +1,27 @@
 package org.example;
 
+import org.example.Command.CommandAction;
+import org.example.Command.CommandList;
+import org.example.Parser.CommandActionParser;
+import org.example.Parser.CommandJsonParser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.io.IOException;
+import java.util.List;
 
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+    public static void main(String[] args) throws IOException {
+
+        ApplicationContext context = SpringApplication.run(Application.class, args);
+        String inputFile = "./src/main/resources/input.json";
+        CommandJsonParser commandJsonParser = new CommandJsonParser();
+        CommandList commandList = commandJsonParser.jsonParse(inputFile);
+        CommandActionParser commandActionParser = new CommandActionParser();
+        List<CommandAction> actions = commandActionParser.commandsParse(commandList);
+        Simulation simulation = new Simulation(actions);
+        simulation.run();
     }
 }
