2f10fc3ba7fd27bcdd69f341a00354244c9b6171
nczajak <nczajak@student.agh.edu.pl>
2025-07-27 20:48:45 +0200

    some configuration

--- src/main/java/org/example/ApplicationController.java
+++ src/main/java/org/example/ApplicationController.java
@@ -1,7 +1,14 @@
 package org.example;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ApplicationController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "index.html";
+    }
 
 }
